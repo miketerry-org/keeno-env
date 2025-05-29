59244cd83f3762dddd4e1632c7d3e460c3859378
Mike Terry <support@miketerry.org>
2025-05-29 13:13:47 -0500

    added loadEnvFiles function

--- index.js
+++ index.js
@@ -5,5 +5,10 @@
 // load all necessary modules
 const loadEncryptKey = require("./lib/loadEncryptKey");
 const loadEnvFile = require("./lib/loadEnvFile");
-const mergeIntoProcessEnv = require("./lib/mergeIntoProcessEnv");
-module.exports = { loadEncryptKey, loadEnvFile, mergeIntoProcessEnv };
+const loadEnvFiles = require("./lib/loadEnvFiles");
+
+module.exports = {
+  loadEncryptKey,
+  loadEnvFile,
+  loadEnvFiles,
+};
